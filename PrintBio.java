dee4b115d96472ebf2c78145c82f562d96687885
MHGani <71458804+MHGani@users.noreply.github.com>
2020-09-17 19:05:25 +0100

    Add files via upload

---
+++ PrintBio.java
@@ -0,0 +1,13 @@
+package apollo.exercises.ch01_output;
+
+public class PrintBio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My name is Mohammed H Gani\n" +
+				"I have previously worked as a BDE\n" + 
+				"My favorite Android App is Netflix\n");
+	}
+
+}
---
+++ printBasicInfo.java
@@ -0,0 +1,14 @@
+package apollo.exercises.ch03_methods;
+
+public class printBasicInfo {
+	public void nameAndemail() {
+	
+		String name1= "Name: David Ball | Email: david.ball@gmail.com";
+		String name2= "Name: James Dean | Email: james.dean@gmail.com";
+		
+		System.out.println(name1);
+		System.out.println(name2);
+
+	}
+
+}
