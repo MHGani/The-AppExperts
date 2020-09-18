ec6b28a2e7ef03999f36aee62e81e8b6d8e85f0b
Hussain <Hussain@MohammedGani>
2020-09-18 14:23:09 +0100

    exercises

---
+++ intro_java_exercises-master/intro_java_exercises-master/src/apollo/exercises/ch01_output/Ex3_PrintQuote.java
@@ -0,0 +1,34 @@
+package apollo.exercises.ch01_output;
+
+// This is a program that prints an interesting quote.
+public class Ex3_PrintQuote {
+
+	// This is the main method that is executed as
+	// soon as the program starts.
+	public static void main(String[] args) {
+	
+		
+		// Print an interesting quote from a famous person.
+		// The quote should contain quotation marks and attribution.
+		// 
+		// HINT: To print out quotes you need to escape the inner quotes:
+		//    
+		//    "\"This is printed in quotes\" - Foo"
+		//
+		
+		System.out.println(" \"The first step toward success is taken when you refuse \n  to be a captive of the environment in which you first find yourself.\" - Mark Caine  ");
+		
+	}
+
+}
+
+/*
+ * 
+ * SAMPLE OUTPUT:
+ *  
+ *   "Have you ever noticed that anybody driving slower than
+ *      you is an idiot, and anyone going faster than you is a maniac?" - George Carlin
+ *   
+ *   Output screenshot: http://d.pr/i/tEYj
+ *    
+ */
